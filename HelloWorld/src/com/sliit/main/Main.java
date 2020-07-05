cee628e98c8a58ca0be89330dc65ed2177b9ff65
KasunHewagama <44252824+KasunHewagama@users.noreply.github.com>
2020-07-05 22:53:07 +0530

    Update Main.java

--- HelloWorld/src/com/sliit/main/Main.java
+++ HelloWorld/src/com/sliit/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first project as SLIIT");
+		System.out.println("This is my first project as SLIIT,Only for the testing...!");
 	}
 
 }
